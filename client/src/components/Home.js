a29d1a51a09342f57f9308f8e75a1713d750a663
Rjjha <rj848jha@gmail.com>
2022-12-23 10:52:00 +0530

    first commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Home.css";
+export default function Home() {
+  return (
+    <div className='header'>
+       <div className='headerTitles'>
+          <span className='headerTitlesm'>Blogs Duniya</span>
+          <span className='headerTitlelg'>My Blog</span>
+       </div>
+       <img className='headerImage' src="https://cdn.unenvironment.org/styles/article_billboard_image/s3/2022-03/externsteine-ga1650e32b_1280.jpg?itok=yL5hN4Bh" alt=""/>
+    </div>
+  )
+}
